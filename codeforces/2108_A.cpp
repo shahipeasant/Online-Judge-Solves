e7290d0a3c5fc1b0757e61f8b6a5ce04d5de0404
shahipeasant <005kaziahnaf@gmail.com>
2025-05-29 04:44:49 +0600

    feat(CF_2108_A): total number of f(p)s is total number of even numbers from 0 to n^2/2

---
+++ codeforces/2108_A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int _;
+    cin >> _;
+    while(_--){
+        long long n;
+        cin >> n;
+        cout << floor((n*n)/4)+1 << endl;
+    }
+
+    return 0;
+}
